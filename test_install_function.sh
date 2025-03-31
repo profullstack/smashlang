d28b1b6c9575f8be3e5c2a8590325f6a0823819e
Anthony Ettinger <anthony@chovy.com>
2025-03-30 22:54:58 -0700

    fix install

---
+++ test_install_function.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Source the install.sh script to get access to its functions
+source ./install.sh
+
+# Call the install_linux function directly
+install_linux true
+
+echo "Test completed successfully!"
