941d2cc41af3c1d37faba7bbb0ca21054a9d0fa0
Anthony Ettinger <anthony@chovy.com>
2025-03-30 23:42:02 -0700

    remove old files

--- test_install_function.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Source the install.sh script to get access to its functions
-source ./install.sh
-
-# Call the install_linux function directly
-install_linux true
-
-echo "Test completed successfully!"
